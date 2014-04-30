cd63c87d5355bab14c022b1dd53d4075fb616e21
Darrell Cardwell <darrell.cardwell@gmail.com>
2014-04-30 17:21:30 -0400

    Faith connection app first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,56 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require turbolinks
+//= require_tree .
+
+$(function(){ 
+	$(document).foundation(); 
+	$('#kwiks-slideshow').kwicks({
+		maxSize: '60%',
+		behavior: 'menu',
+		autoResize: true,
+		spacing: 0
+	});
+	$("#slideshow").on('expand-complete.kwicks', function(e, data){
+		$(data.expanded).children("div").fadeIn(200);		
+		var c = $("#middleContainer");		
+		if(data.index !== -1){
+			$(".slide").fadeOut(100).delay(200);			
+			$("#slide"+data.index.toString()).fadeIn(300);
+		}
+		
+	});
+	$("#slider").on('expand.kwicks', function(e, data) {
+		$(data.oldExpanded).children("div").hide();		
+		$(data.collapsed).children("div").hide();	
+	});
+	
+	$("#connect-button").click(function(e){
+		if($(this).attr("data-open") === "false"){
+			$("#login-form").slideDown(300);
+			$(this).animate({"border-bottom-left-radius":0, "border-bottom-right-radius":0, "-webkit-border-bottom-left-radius": 0, "-webkit-border-bottom-right-radius": 0}, 300);
+			//$(this).css({"border-bottom-left-radius":"0px","-webkit-border-bottom-right-radius":"0px"});
+			$(this).attr("data-open", "true");
+		}else{
+			$("#login-form").slideUp(300);
+			$(this).animate({"border-bottom-left-radius":10, "border-bottom-right-radius":10, "-webkit-border-bottom-left-radius": 10, "-webkit-border-bottom-right-radius": 10}, 300);
+			//$(this).css({"border-bottom-left-radius":"10px","-webkit-border-bottom-right-radius":"10px"});
+			$(this).attr("data-open", "false");			
+		}
+		e.preventDefault();
+	});
+	
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FaithApp::Application.config.secret_key_base = '416d39ba04c0a498ab3e93cdd56d0dd5a3dee99d05915460dabd19b07df9b20ad4b3e451aecfd32950a85cfe53c63392f4e469a4a2e788adff33149c86ad8fbe'
